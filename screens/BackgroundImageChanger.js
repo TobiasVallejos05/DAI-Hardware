8212c1991485391d39a283bfb1711d81d95c366e
TobiasVallejos05 <tochu0505@gmail.com>
2022-10-02 20:10:24 -0300

    02/10

--- screens/BackgroundImageChanger.js
+++ screens/BackgroundImageChanger.js
@@ -1,13 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Image, FlatList, SafeAreaView, TouchableOpacity, StyleSheet, StatusBar, Button } from 'react-native';
 import { useContextState } from '../contextState'
+import ImagePicker, { launchImageLibrary, launchCamera } from 'react-native-image-picker';
 
 const BackgroundImageChanger = ({navigation}) => {
+  
+  const [image, setImage] = useState('https://via.placeholder.com/640x480');
+
+  const selectImage = () => {
+
+    const options = {
+      title: "Selecciona una imagen",
+      storageOptions: {
+        skipBackup: true,
+        path: "images",
+      }
+    }
+
+    launchImageLibrary(options, response => {
+
+      if(response.errorCode){
+        console.log(response.errorMessage);
+      } else if(response.didCancel){
+        console.log("La selección de imagen fue cancelada");
+      } else {
+        const path = response.assets[0].uri
+        setImage(path)
+      } 
+    })
+
+  }
+
+  const takePicture = () => {
+
+    const options = {
+      title: "Sacar foto",
+      storageOptions: {
+        skipBackup: true,
+        path: "images",
+      },
+      includeBase64: true,
+    }
+
+    launchCamera(options, response => {
+
+      if(response.errorCode){
+        console.log(response.errorMessage);
+      } else if(response.didCancel){
+        console.log("El usuario canceló la fotografía");
+      } else {
+        const uri = response.assets[0].uri
+        setImage(uri)
+      }
+    })
+  }
 
   return (
 
     <View style={styles.container}>
         <Text style={styles.title}>Cambio de Imagen de Fondo</Text>
+        <View style={styles.buttonsPosition}>
+        <Button
+          title="Seleccionar imagen de la galería"
+          onPress = {() => selectImage()}
+        />
+        <Button 
+          title="Tomar foto de la cámara"
+          onPress = {() => takePicture()}
+        />
+        </View>
+        <Image 
+          style = {{
+            height: 640,
+            width: 480,
+            alignSelf: "center"
+          }}
+          source = {{ uri: image }}
+        />
     </View> 
   ); 
 }
@@ -22,6 +91,11 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: 500,
         marginTop: 20
+    },
+    buttonsPosition: {
+        flexDirection: "row",
+        marginTop: 20,
+        marginBottom: 20,
     },
 });
 
--- screens/Identifier.js
+++ screens/Identifier.js
@@ -1,14 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Image, FlatList, SafeAreaView, TouchableOpacity, StyleSheet, StatusBar, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { useContextState } from '../contextState'
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
 const Identifier = ({navigation}) => {
+  
+  const [permission, setPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Todavía no hay nada escaneado');
+
+  const cameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setPermission(status == 'granted')
+    })
+  };
+
+  useEffect(() => {
+    cameraPermission();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData " + data)
+  };
+
+  if (permission === null) {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.title}>Esperando respuesta...</Text>
+      </View>
+    )
+  };
+
+  if (permission === false) {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.title}>Acceso denegado</Text>
+      </View>
+    )
+  }
 
   return (
 
     <View style={styles.container}>
-        <Text style={styles.title}>Identificación de Cada Aplicación</Text>
-    </View> 
+        <Text style={styles.title}>Acceso permitido</Text>
+        <BarCodeScanner 
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 200, width: 200 }} />
+        <Text>{text}</Text>
+
+        {scanned && <Button title={'¿Desea volver a escanear?'} onPress={() => setScanned(false)} /> }
+    </View>
   ); 
 }
 
@@ -19,8 +63,8 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     title: {
-        fontSize: 40,
-        fontWeight: 500,
+        fontSize: 30,
+        fontWeight: 400,
         marginTop: 20
     },
 });
